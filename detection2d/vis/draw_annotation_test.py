ba0f751bd574fbcc0900315c4b1e6341beef9582
Qin Liu <sjtuliuqin@163.com>
2020-06-18 18:15:35 -0400

    Modify the path

--- detection2d/vis/draw_annotation_test.py
+++ detection2d/vis/draw_annotation_test.py
@@ -6,15 +6,15 @@ from PIL import Image
 from detection2d.vis.draw_annotation import draw_annotation
 
 
-data_dir = '/mnt/projects/CXR_Object/'
+data_dir = '/mnt/projects/CXR_Object'
 OBJECT_SEP = ';'
 ANNOTATION_SEP = ' '
 
-save_fig_folder = '/mnt/projects/CXR_Object/vis'
+save_fig_folder = '/mnt/projects/CXR_Object/results/vis'
 if not os.path.isdir(save_fig_folder):
     os.makedirs(save_fig_folder)
 
-train_csv = os.path.join(data_dir, 'train.csv')
+train_csv = os.path.join(data_dir, 'dataset', 'train.csv')
 labels_tr = pd.read_csv(train_csv, na_filter=False)
 
 # viz
@@ -23,7 +23,7 @@ for idx in range(len(labels_tr)):
     image_name = labels_tr.iloc[idx]['image_name']
     annotation = labels_tr.iloc[idx]['annotation']
 
-    image_path = os.path.join(data_dir, "train", image_name)
+    image_path = os.path.join(data_dir, "data", "train", image_name)
     image = Image.open(image_path).convert("RGB")
     if annotation:
         draw_annotation(image, annotation)
