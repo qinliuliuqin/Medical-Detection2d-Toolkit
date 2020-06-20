bc3d2b2adb78bece112023f4d7e4a869714df9ba
Qin Liu <sjtuliuqin@163.com>
2020-06-20 18:11:25 -0400

    Fix a bug

--- detection2d/det_train.py
+++ detection2d/det_train.py
@@ -90,7 +90,8 @@ def train(config_file, gpu_id):
     )
 
     model_module = importlib.import_module('detection2d.network.' + cfg.net.name)
-    model = model_module.get_detection_model(cfg.dataset.num_classes, cfg.net.pre_trained, image_mean=0, image_std=1)
+    model = model_module.get_detection_model(cfg.dataset.num_classes, cfg.net.pre_trained,
+                                             image_mean=[0, 0, 0], image_std=[1, 1, 1])
 
     if cfg.general.num_gpus > 0:
         torch.cuda.set_device(gpu_id)
