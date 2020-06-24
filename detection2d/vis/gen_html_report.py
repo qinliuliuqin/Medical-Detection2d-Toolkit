482d14b40e1b93d63ab358b55e15bbd519061de7
Qin Liu <sjtuliuqin@163.com>
2020-06-24 12:16:36 -0400

    Add the gen_html_report pipeline

--- detection2d/vis/gen_html_report.py
+++ detection2d/vis/gen_html_report.py
@@ -120,29 +120,44 @@ def gen_row_for_html(usage_flag, image_link_template, error_info_template, docum
     """
     Generate a line of html text contents for labelled cases, in the usage of label checking.
     """
-    case_info = r'<b>Case nunmber</b>:{0}: {1}'.format(image_idx, image_name)
+    case_info = r'<b>Case nunmber</b>:{0}: {1};'.format(image_idx, image_name)
+    image_name_prefix = image_name.split('.')[0]
+    image_name_postfix = image_name.split('.')[1]
 
     if usage_flag == 1:
-        pass
+        labeled_image_name = '{}_labelled.{}'.format(image_name_postfix, image_name_postfix)
+        error_info = error_info_template.format(0)
+
     elif usage_flag == 2:
-        pass
+        labeled_image_name = '{}_labelled.{}'.format(image_name_postfix, image_name_postfix)
+        detected_image_name = '{}_detected.{}'.format(image_name_prefix, image_name_postfix)
+        error_info = error_info_template.format(0, 0, 'TP', 0.0)
+
     else:
-        raise ValueError('Unsupported flag type!')
+        raise ValueError('Unsupported usage flag.')
 
     document_text = add_document_text(document_text, case_info)
-
+    document_text = add_document_text(document_text, error_info)
     document_text += "\n"
     document_text = add_document_text(document_text, "<table border=1><tr>")
-    document_text = add_image(document_text, image_link_template, picture_folder, image_name, width)
-    if usage_flag == 2:
-        pass
+
+    if usage_flag == 1:
+        document_text = add_image(document_text, image_link_template, picture_folder, labeled_image_name, width)
+
+    elif usage_flag == 2:
+        document_text = add_image(document_text, image_link_template, picture_folder, labeled_image_name, width)
+        document_text = add_image(document_text, image_link_template, picture_folder, detected_image_name, width)
+
+    else:
+        raise ValueError('Unsupported usage flag.')
+
     document_text += "\n"
     document_text = add_document_text(document_text, r'</tr></table>')
 
     return document_text
 
 
-def gen_html_report(objects_dict, usage_flag, output_folder):
+def gen_html_report(image_list, objects_dict, usage_flag, output_folder):
     """
     Generate HTML report for object detection.
     :param objects_dict:
@@ -152,38 +167,29 @@ def gen_html_report(objects_dict, usage_flag, output_folder):
     """
 
     labelled_objects_dict = objects_dict[0]
-
     if usage_flag == 2:
         detected_objects_dict = objects_dict[1]
-        assert len(labelled_objects_dict.keys()) == len(detected_objects_dict.keys())
 
     document_text = r'"<h1>check annotations:</h1>"'
-    for image_idx, image_name in enumerate(labelled_objects_dict.keys()):
+    for image_idx, image_name in enumerate(image_list):
         print("Processing image {}.".format(image_name))
         image_link_template = r"<div class='content'><img border=0  src= '{0}'  hspace=1  width={1} class='pic'></div>"
         error_info_template = r'<b>Labelled</b>: {};'
         document_text += "\n"
 
         if usage_flag == 1:
-            document_text = gen_row_for_html(usage_flag,
-                                             image_link_template,
-                                             error_info_template,
-                                             document_text,
-                                             image_name,
-                                             image_idx,
-                                             None)
+            document_text = gen_row_for_html(usage_flag, image_link_template,
+                error_info_template, document_text, image_name, image_idx, None)
 
         elif usage_flag == 2:
-            pass
+            error_info_template += r'<b>Detected</b>: {};'
+            error_info_template += r'<b>Type</b>: {};'
+            error_info_template += r'<b>Max Det. Prob.</b>: {};'
+
+            document_text = gen_row_for_html(usage_flag, image_link_template,
+                error_info_template, document_text, image_name, image_idx, None)
         else:
             raise ValueError('Undefined usage flag!')
-
-        if usage_flag == 1:
-            pass
-        elif usage_flag == 2:
-            pass
-        else:
-            raise ValueError('Undefined usage float!')
 
     html_report_name = 'result_analysis.html'
     html_report_path = os.path.join(output_folder, html_report_name)
--- detection2d/vis/gen_html_report_test.py
+++ detection2d/vis/gen_html_report_test.py
@@ -1,21 +1,19 @@
 import argparse
-import glob
 import os
 import pandas as pd
 
 from detection2d.vis.gen_html_report import gen_html_report
+from detection2d.vis.gen_images import gen_plane_images
 
 
 def parse_and_check_arguments():
     """
     Parse input arguments and raise error if invalid.
     """
-    default_image_folder = '/mnt/projects/CT_Dental/data'
-    default_label_folder = '/mnt/projects/CT_Dental/landmark'
-    default_detection_folder = '/mnt/projects/CT_Dental/results/model_0514_2020//batch_3/epoch_1000/test_set'
-    default_resolution = [1.5, 1.5, 1.5]
-    default_contrast_range = None
-    default_output_folder = '/mnt/projects/CT_Dental/results/model_0514_2020//batch_3/epoch_1000/test_set/html_report'
+    default_image_folder = '/mnt/projects/CXR_Object/data/dev'
+    default_label_file = '/mnt/projects/CXR_Object/dataset/dev.csv'
+    default_detection_file = '/mnt/projects/CXR_Object/results/model_0622_2020/contrast_flip/dev/localization.csv'
+    default_output_folder = '/mnt/projects/CXR_Object/results/model_0622_2020/html_report'
     default_generate_pictures = False
 
     parser = argparse.ArgumentParser(
@@ -23,18 +21,12 @@ def parse_and_check_arguments():
     parser.add_argument('--image_folder', type=str,
                         default=default_image_folder,
                         help='Folder containing the source data.')
-    parser.add_argument('--label_folder', type=str,
-                        default=default_label_folder,
+    parser.add_argument('--label_file', type=str,
+                        default=default_label_file,
                         help='A folder where CSV files containing labelled landmark coordinates are stored.')
-    parser.add_argument('--detection_folder', type=str,
-                        default=default_detection_folder,
+    parser.add_argument('--detection_file', type=str,
+                        default=default_detection_file,
                         help='A folder where CSV files containing detected or baseline landmark coordinates are stored.')
-    parser.add_argument('--resolution', type=list,
-                        default=default_resolution,
-                        help="Resolution of the snap shot images.")
-    parser.add_argument('--contrast_range', type=list,
-                        default=default_contrast_range,
-                        help='Minimal and maximal value of contrast intensity window.')
     parser.add_argument('--output_folder', type=str,
                         default=default_output_folder,
                         help='Folder containing the generated snapshot images.')
@@ -47,13 +39,27 @@ def parse_and_check_arguments():
 
 if __name__ == '__main__':
 
-    data_folder = '/mnt/projects/CXR_Object/train'
-    labels_file = '/mnt/projects/CXR_Object/train.csv'
-    labels_df = pd.read_csv(labels_file, na_filter=False)
-    print(f'{len(os.listdir(data_folder))} pictures in {data_folder}')
+    args = parse_and_check_arguments()
 
+    labels_df = pd.read_csv(args.label_file, na_filter=False)
     labels = labels_df.loc[labels_df['annotation'].astype(bool)].reset_index(drop=True)
-    img_class_dict_tr = dict(zip(labels.image_name, labels.annotation))
+    labels_dict = dict(zip(labels.image_name, labels.annotation))
 
-    output_folder = '/mnt/projects/CXR_Object/vis'
-    gen_html_report([img_class_dict_tr], 1, output_folder)
+    image_list = labels_df['image_name'].to_list()
+
+    usage_flag = 1
+    preds_dict = {}
+    if os.path.isfile(args.detection_file):
+        usage_flag = 2
+        preds_df = pd.read_csv(args.detection_file, na_filter=False)
+        preds = preds_df.loc[preds_df['prediction'].astype(bool)].reset_index(drop=True)
+        preds_dict = dict(zip(preds.image_name, preds.prediction))
+
+    if not os.path.isdir(args.output_folder):
+        os.makedirs(args.output_folder)
+
+    gen_html_report(image_list, [labels_dict, preds_dict], usage_flag, args.output_folder)
+
+    if args.generate_pictures:
+        print('Start generating planes for the labelled landmarks.')
+        gen_plane_images(args.image_folder, labels_dict, preds_dict, args.output_folder)
